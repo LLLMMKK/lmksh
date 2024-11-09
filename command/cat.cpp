563716a1f050cc33623331bbb24cf4367241311e
llmmkk <34044536@qq.com>
2024-11-09 20:01:12 +0800

    更改 cat 去掉多余换行符，不再检测 '-' 文件

--- command/cat.cpp
+++ command/cat.cpp
@@ -21,18 +21,19 @@ int main(int argc, char *argv[], char *envp[]) {
         buffer[bytesRead] = '\0';
         printf("%s", buffer);
       }
-    }
-    int fd = open(argv[i], O_RDONLY);
-    if (fd == -1)
-      printf("No file named %s.\n", argv[i]);
-    else {
-      char buf[512000];
-      ssize_t rd = read(fd, buf, 512000);
-      close(fd);
-      if (rd == -1)
-        printf("Failed to read the file %s.\n", argv[i]);
-      else
-        printf("%s\n", buf);
+    } else {
+      int fd = open(argv[i], O_RDONLY);
+      if (fd == -1)
+        printf("No file named %s.", argv[i]);
+      else {
+        char buf[512000];
+        ssize_t rd = read(fd, buf, 512000);
+        close(fd);
+        if (rd == -1)
+          printf("Failed to read the file %s.", argv[i]);
+        else
+          printf("%s", buf);
+      }
     }
   }
   if (isnull) {
